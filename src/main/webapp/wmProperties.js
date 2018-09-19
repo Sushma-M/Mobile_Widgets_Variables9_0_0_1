a3bdc36f9520d2aed1de7cf2b3e7c74cda3ca6d6
Imtiyaz Mohammad <imtiyaz.mohammad@wavemaker.com>
2018-09-19 08:27:28 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobile_Widgets_Variables",
+  "homePage" : "Main",
+  "name" : "Mobile_Widgets_Variables",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
